5a4469d2ebc347c6507402c143cfed64fe54c4c2
TomasTNunes <tomastrindadenunes@outlook.com>
2025-08-09 16:20:41 +0100

    refactor: change code flow and threads initialization

--- src/modules/camera.py
+++ src/modules/camera.py
@@ -1,13 +1,15 @@
 import cv2
 import time
 import threading
+from queue import Queue
 from modules.stream import StreamServer
+from modules.recording import RecordingManager
 from logger_setup import logger
 
 class CameraReader:
     """
     Camera Module Class.
-    Handles camera initialization, frame reading and continiously feeding it to Processing and Stream Modules.
+    Handles camera initialization, frame reading and jpeg encoding, and continiously feeding it to Stream, RecordingManager and Motion Modules.
     Also provides methods to draw clock and FPS on frames.
     Frame Reading using time-based throttling, reading all frames but displaying at a fixed rate.
     Designed to be used with OpenCV.
@@ -23,6 +25,7 @@ class CameraReader:
         self.camera_name = camera_name
         self.target_fps = target_fps
         self.show_fps = show_fps
+        self.stream_quality = stream_quality
 
         # Camera Thread
         self._camera_thread = threading.Thread(
@@ -45,28 +48,61 @@ class CameraReader:
             self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
         if source_fps:
             self.cap.set(cv2.CAP_PROP_FPS, source_fps)
+        
+        # Initialize raw frames queue 
+        max_stream_queue_size = 10  # Allow some buffer for frames for stream (lower this if RAM usage is too high) (raw frames are heavy)
+        self.frame_queue = Queue(maxsize=max_stream_queue_size)
+
+        # Frame dispatcher Thread
+        self._frame_dispatcher_thread = threading.Thread(
+            target=self._frame_dispatcher,
+            daemon=True
+        )
+        self._frame_dispatcher_stop_event = threading.Event()
+
 
         # Initialize StreamServer Class Module
-        self.stream_server = StreamServer(camera_name, camera_name_norm, port, target_fps, stream_quality)
+        self.stream_server = StreamServer(
+            camera_name=camera_name, 
+            camera_name_norm=camera_name_norm, 
+            port=port, 
+            target_fps=target_fps
+        )
 
-        # Initialize motion frames queue
-        # max_motion_queue_size = 10  # Allow some buffer for frames for stream (lower this if RAM usage is too high)
-        # self.motion_frame_queue = Queue(maxsize=max_motion_queue_size)
+        # Initialize RecordingManager Class Module
+        self.recording_manager = RecordingManager(
+            camera_name=camera_name,
+            camera_name_norm=camera_name_norm,
+            target_fps=target_fps
+        )
     
     def start(self):
         """
-        Starts the camera reader thread.
-        """
-        self._camera_thread.start()
+        Starts the camera threads.
+        """ 
+        self._frame_dispatcher_thread.start() # Start frame dispatcher thread
+        self._camera_thread.start() # Start Camera reader thread
+        self.stream_server.start() # Start streaming thread
+        self.recording_manager.start() # Start recording thread
     
     def stop(self):
         """
         Stops the camera reader thread, and child threads (stream, motion, etc).
         """
-        self.stream_server.recording_manager.stop()  # Stop recording manager thread
+        self.recording_manager.stop() # Stop recording manager thread
+        self._frame_dispatcher_stop_event.set()
         self._camera_stop_event.set()
+        self._frame_dispatcher_thread.join()
         self._camera_thread.join()
         logger.info(f"Camera '{self.camera_name}' thread stopped.")
+    
+    def _close_camera_reader(self):
+        """
+        Releases the camera and closes correspondant OpenCV windows.
+        """
+        if self.cap.isOpened():
+            self.cap.release()
+            logger.info(f"Camera '{self.camera_name}' released.")
 
     def _run(self):
         """
@@ -75,6 +111,7 @@ class CameraReader:
         Starts Stream Thread and Motion Process.
         Uses time-based throttling.
         """
+        # Get camera actual info
         actual_fourcc = int(self.cap.get(cv2.CAP_PROP_FOURCC))
         actual_fmt = "".join([chr((actual_fourcc >> 8 * i) & 0xFF) for i in range(4)])
         source_fps = int(self.cap.get(cv2.CAP_PROP_FPS))
@@ -82,7 +119,6 @@ class CameraReader:
 (Source_Format: {actual_fmt}, Source_FPS: {source_fps}, \
 Target_FPS: {self.target_fps}, Width: {int(self.cap.get(cv2.CAP_PROP_FRAME_WIDTH))}, \
 Height: {int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))})")
-        self.stream_server.start() # Start streaming thread
 
         # Compute sleep time based on source FPS
         if source_fps and source_fps >= 1:
@@ -130,21 +166,50 @@ Height: {int(self.cap.get(cv2.CAP_PROP_FRAME_HEIGHT))})")
                     # Draw name and time-stamps
                     frame = self._draw_frame_info(frame, now)
 
-                # Write raw frame to stream server queue
-                self.stream_server.write(frame)
+                # Write raw frame to frame queue
+                self._write(frame)
             
             time.sleep(sleep_time)  # Sleep to reduce CPU usage
 
         # Close camera and release resources
         self._close_camera_reader()
+    
+    def _write(self, frame: bytes):
+        """
+        Writes a raw frame to the frame queue.
+        If the queue is full, the frame is dropped.
+        """
+        try:
+            self.frame_queue.put_nowait(frame)
+        except:
+            pass  # Drop frame if queue is full 
+                    # (Might be a good idea to relpace frame by oldest instead of dropping it, 
+                    # should reduce latency build up in spike scenarios)
+    
+    def _frame_dispatcher(self):
+        """
+        Seperate thread loop to read raw frame from queue and encoding it to jpeg.
+        It will then serve encoded frame to Stream Class Module and RecordingManager Class Module.
+        It will serve a tuple (raw, encoded) frames to Motion Class Module.
+        """
+        while not self._frame_dispatcher_stop_event.is_set():
+            frame = self.frame_queue.get()
+            if frame is None:
+                 continue
+            
+            # Encode frame as JPEG
+            ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), self.stream_quality])
+            if not ret:
+                continue
 
-    def _close_camera_reader(self):
-        """
-        Releases the camera and closes correspondant OpenCV windows.
-        """
-        if self.cap.isOpened():
-            self.cap.release()
-            logger.info(f"Camera '{self.camera_name}' released.")
+            # JPEG NumPy array to bytes
+            jpeg_bytes = jpeg.tobytes()
+
+            # Feed Encoded frame to Stream
+            self.stream_server.write(jpeg_bytes)
+
+            # Feed Encoded frame to RecordingManager
+            self.recording_manager.write(jpeg_bytes)
 
     def _draw_frame_info(self, frame: bytes, now: float, fps: float = None):
         """
--- src/modules/stream.py
+++ src/modules/stream.py
@@ -2,9 +2,7 @@ import cv2
 import threading
 import time
 import socket
-from queue import Queue
 from flask import Flask, Response
-from modules.recording import RecordingManager
 from logger_setup import logger
 
 class StreamServer:
@@ -12,17 +10,16 @@ class StreamServer:
     Stream Module Class.
     Handles streaming of frames from the camera to a specified port.
     Designed to be used with Flask for web streaming.
-    Feeds encoded frames to RecordingManager Module.
     """
 
-    def __init__(self, camera_name: str, camera_name_norm: str, port: int, target_fps: int, stream_quality: int):
+    def __init__(self, camera_name: str, camera_name_norm: str, port: int, target_fps: int):
         """
         Initializes the StreamServer with required parameters.
         """
         self.camera_name = camera_name
+        self.camera_name_norm = camera_name_norm
         self.port = port
         self.target_fps = target_fps
-        self.stream_quality = stream_quality
 
         # Stream Thread
         self._stream_thread = threading.Thread(
@@ -30,74 +27,28 @@ class StreamServer:
             daemon=True
         )
 
-        # Initialize stream raw frames queue 
-        max_stream_queue_size = 10  # Allow some buffer for frames for stream (lower this if RAM usage is too high) (raw frames are heavy)
-        self.frame_queue = Queue(maxsize=max_stream_queue_size)
-
-        # Initialize variables for stream frame queue reader tread
-        self._latest_frame = None
+        # Latest frame control variables
         self._latest_frame_lock = threading.Lock()
-        self._frame_dispatcher_thread = threading.Thread(
-            target=self._frame_dispatcher,
-            daemon=True
-        )
-
-        # Initialize RecordingManager Class Module
-        self.recording_manager = RecordingManager(
-            camera_name=camera_name,
-            camera_name_norm=camera_name_norm,
-            target_fps=target_fps
-        )
+        self._latest_frame = None
     
     def write(self, frame: bytes):
         """
-        Writes a raw frame to the stream queue.
-        If the queue is full, the frame is dropped.
+        Updates latest frame.
         """
-        try:
-            self.frame_queue.put_nowait(frame)
-        except:
-            pass  # Drop frame if queue is full 
-                    # (Might be a good idea to relpace frame by oldest instead of dropping it, 
-                    # should reduce latency build up in spike scenarios)
+        with self._latest_frame_lock:
+            self._latest_frame = frame
 
     def start(self):
         """
-        Starts the stream server thread and encoded frame dispatcher thread.
+        Starts the stream server thread.
         """
-        self._frame_dispatcher_thread.start()
         self._stream_thread.start()
-    
-    def _frame_dispatcher(self):
-        """
-        Seperate thread loop to read raw frame from queue, encoding it to jpeg and serve it to all clients and Recorfing Manger Class Module.
-        """
-        while True:
-            frame = self.frame_queue.get()
-            if frame is None:
-                 continue
-            
-            # Encode frame as JPEG
-            ret, jpeg = cv2.imencode('.jpg', frame, [int(cv2.IMWRITE_JPEG_QUALITY), self.stream_quality])
-            if not ret:
-                continue
-
-            # JPEG NumPy array to bytes
-            jpeg_bytes = jpeg.tobytes()
-
-            # Save for clients
-            with self._latest_frame_lock:
-                self._latest_frame = jpeg_bytes
-
-            # Feed Encoded frame to RecordingManager
-            self.recording_manager.write(jpeg_bytes)
 
     def _run(self):
         """
         Run Flask app in a seperate thread.
         """
         logger.info(f"Starting Flask stream server for {self.camera_name} on port {self.port} (http://{self._get_local_ip()}:{self.port}).")
-        self.recording_manager.start()  # Start recording manager thread
         app = Flask(__name__)
 
         def generate():
@@ -136,7 +87,8 @@ class StreamServer:
         except Exception as e:
             logger.error(f'Error running flask app for camera {self.camera_name}: {e}')
     
-    def _get_local_ip(self):
+    @staticmethod
+    def _get_local_ip():
         """
         Returns the local IP address of the machine.
         """
